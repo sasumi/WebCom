29534bfef0e480e01c44f305eb248e9887580c49
sasumi <cnsasumi@gmail.com>
2023-04-04 21:26:35 +0800

    upd

--- src/Widget/Menu.js
+++ src/Widget/Menu.js
@@ -18,7 +18,7 @@ let LAST_MENU_EL = null;
 insertStyleSheet(`
 	.${CTX_CLASS_PREFIX} {z-index:${CTX_Z_INDEX}; position:fixed;}
 	.${CTX_CLASS_PREFIX},
-	.${CTX_CLASS_PREFIX} ul {position:absolute; padding: 0.5em 0; list-style:none; box-shadow:1px 1px 10px 0px #44444457;border-radius:4px;background:#fff;min-width:12em; display:none;}
+	.${CTX_CLASS_PREFIX} ul {position:absolute; padding: 0.5em 0; list-style:none; backdrop-filter:blur(5px); box-shadow:1px 1px 10px 0px #44444457;border-radius:4px;background:#ffffffd9;min-width:12em; display:none;}
 	.${CTX_CLASS_PREFIX} ul {left:100%; top:0;}
 	.${CTX_CLASS_PREFIX} li:not([disabled]):hover>ul {display:block;}
 	.${CTX_CLASS_PREFIX} li[role=menuitem] {padding:0 1em; line-height:1; position:relative; min-height:2em; display:flex; align-items:center; background: transparent;user-select:none;opacity: 0.5; cursor:default;}
@@ -26,8 +26,8 @@ insertStyleSheet(`
 	.${CTX_CLASS_PREFIX} li[role=menuitem]:not([disabled]) {cursor:pointer; opacity:1;}
 	.${CTX_CLASS_PREFIX} li[role=menuitem]:not([disabled]):hover {background-color: #eeeeee9c;text-shadow: 1px 1px 1px white;opacity: 1;}
 	.${CTX_CLASS_PREFIX} .has-child:after {content:"\\e73b"; font-family:${Theme.IconFont}; zoom:0.7; position:absolute; right:0.5em; color:gray;}
-	.${CTX_CLASS_PREFIX} .has-child:not([disabled]):hover:after {color:black}s
-	.${CTX_CLASS_PREFIX} .sep {margin:0.25em 0.5em;border-bottom:1px solid #ddd;}
+	.${CTX_CLASS_PREFIX} .has-child:not([disabled]):hover:after {color:black}
+	.${CTX_CLASS_PREFIX} .sep {margin:0.25em 0.5em;border-bottom:1px solid #eee;}
 	.${CTX_CLASS_PREFIX} .caption {padding-left: 1em;opacity: 0.7;user-select: none;display:flex;align-items: center;}
 	.${CTX_CLASS_PREFIX} .caption:after {content:"";flex:1;border-bottom: 1px solid #ccc;margin: 0 0.5em;padding-top: 3px;}
 	.${CTX_CLASS_PREFIX} li i {--size:1.2em; display:block; width:var(--size); height:var(--size); max-width:var(--size); margin-right:0.5em;} /** icon **/
