d6f14a43bdc690fac1bb9dd1916551bab75c19f0
sasumi <cnsasumi@gmail.com>
2023-04-11 16:30:37 +0800

    upd

--- dist/webcom.browser.js
+++ dist/webcom.browser.js
@@ -2516,70 +2516,6 @@ var WebCom = (function (exports) {
 		}
 	}
 
-	/**
-	 * copy text
-	 * @param {String} text
-	 * @param {Boolean} silent 是否在不兼容是进行提醒
-	 * @returns {boolean} 是否复制成功
-	 */
-	const copy = (text, silent = false) => {
-		let txtNode = createDomByHtml('<textarea readonly="readonly">', document.body);
-		txtNode.style.cssText = 'position:absolute; left:-9999px;';
-		let y = window.pageYOffset || document.documentElement.scrollTop;
-		txtNode.addEventListener('focus', function(){
-			window.scrollTo(0, y);
-		});
-		txtNode.value = text;
-		txtNode.select();
-		try{
-			let succeeded = document.execCommand('copy');
-			!silent && Toast.showSuccess(trans('复制成功'));
-			return succeeded;
-		}catch(err){
-			Toast.showWarning(trans('请按键: Ctrl+C, Enter复制内容'), text);
-			console.error(err);
-		} finally{
-			txtNode.parentNode.removeChild(txtNode);
-		}
-		return false;
-	};
-
-	/**
-	 * Copy formatted html content
-	 * @param html
-	 * @param silent
-	 */
-	const copyFormatted = (html, silent = false) => {
-		// Create container for the HTML
-		let container = createDomByHtml(`
-		<div style="position:fixed; pointer-events:none; opacity:0;">${html}</div>
-	`, document.body);
-
-		// Detect all style sheets of the page
-		let activeSheets = Array.prototype.slice.call(document.styleSheets)
-			.filter(function(sheet){
-				return !sheet.disabled;
-			});
-
-		// Copy to clipboard
-		window.getSelection().removeAllRanges();
-
-		let range = document.createRange();
-		range.selectNode(container);
-		window.getSelection().addRange(range);
-
-		document.execCommand('copy');
-		for(let i = 0; i < activeSheets.length; i++){
-			activeSheets[i].disabled = true;
-		}
-		document.execCommand('copy');
-		for(let i = 0; i < activeSheets.length; i++){
-			activeSheets[i].disabled = false;
-		}
-		document.body.removeChild(container);
-		!silent && Toast.showSuccess(trans('复制成功'));
-	};
-
 	let masker = null;
 	let CSS_CLASS = Theme.Namespace+'-masker';
 
@@ -3271,6 +3207,70 @@ var WebCom = (function (exports) {
 			return parent.DialogManager.findById(id);
 		}
 	}
+
+	/**
+	 * copy text
+	 * @param {String} text
+	 * @param {Boolean} silent 是否在不兼容是进行提醒
+	 * @returns {boolean} 是否复制成功
+	 */
+	const copy = (text, silent = false) => {
+		let txtNode = createDomByHtml('<textarea readonly="readonly">', document.body);
+		txtNode.style.cssText = 'position:absolute; left:-9999px;';
+		let y = window.pageYOffset || document.documentElement.scrollTop;
+		txtNode.addEventListener('focus', function(){
+			window.scrollTo(0, y);
+		});
+		txtNode.value = text;
+		txtNode.select();
+		try{
+			let succeeded = document.execCommand('copy');
+			!silent && Toast.showSuccess(trans('复制成功'));
+			return succeeded;
+		}catch(err){
+			console.error(err);
+			Dialog.prompt('复制失败，请手工复制', {initValue:text});
+		} finally{
+			txtNode.parentNode.removeChild(txtNode);
+		}
+		return false;
+	};
+
+	/**
+	 * Copy formatted html content
+	 * @param html
+	 * @param silent
+	 */
+	const copyFormatted = (html, silent = false) => {
+		// Create container for the HTML
+		let container = createDomByHtml(`
+		<div style="position:fixed; pointer-events:none; opacity:0;">${html}</div>
+	`, document.body);
+
+		// Detect all style sheets of the page
+		let activeSheets = Array.prototype.slice.call(document.styleSheets)
+			.filter(function(sheet){
+				return !sheet.disabled;
+			});
+
+		// Copy to clipboard
+		window.getSelection().removeAllRanges();
+
+		let range = document.createRange();
+		range.selectNode(container);
+		window.getSelection().addRange(range);
+
+		document.execCommand('copy');
+		for(let i = 0; i < activeSheets.length; i++){
+			activeSheets[i].disabled = true;
+		}
+		document.execCommand('copy');
+		for(let i = 0; i < activeSheets.length; i++){
+			activeSheets[i].disabled = false;
+		}
+		document.body.removeChild(container);
+		!silent && Toast.showSuccess(trans('复制成功'));
+	};
 
 	/**
 	 * 通过 src 加载图片
