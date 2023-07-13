132406b2c52516ce93f42aec0fb57910e216d164
sasumi <cnsasumi@gmail.com>
2023-07-13 13:17:55 +0800

    upd

--- test/array.js
+++
@@ -1,3 +0,0 @@
-await import("../dist/webcom.cjs.js");
-let a = ['a', 'b', 'c', 'd', 'a'];
-console.log(arrayDistinct(a));
--- test/assets/nav.js
+++ test/assets/nav.js
@@ -5,12 +5,14 @@ const nav = {
 	'imgpreview.html' : 'Image Preview',
 	'toast.html' : 'Toast',
 	'tip.html' : 'Tip',
+	'fullscreen.html': 'fullscreen',
 	'html_cut.html' : 'Html Cut',
 	'menu.html':'Menu',
+	'novice_guide.html':'Novice Guide',
 	'select.html':'select'
 }
 let html = `
-<link rel="stylesheet" href="test.css">
+<link rel="stylesheet" href="assets/style.css">
 <ul class="nav">`;
 for(let i in nav){
 	html += `<li><a href="${i}">${nav[i]}</a></li>`;
