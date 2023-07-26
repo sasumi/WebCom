42f4b0f4329775a43f2965da9212972c9d03565c
sasumi <cnsasumi@gmail.com>
2023-07-26 16:26:41 +0800

    upd

--- dist/webcom.es.js
+++ dist/webcom.es.js
@@ -3860,16 +3860,16 @@ const copyFormatted = (html, silent = false) => {
  * 参数：
  * *[data-copy-content]
  * *[data-copy-type] type 为 html 时表示复制内容为HTML
+ * 使用举例：
+ * <input type="button" value="复制链接” data-copy-content="http://abc.com" data-component="copy"/>
  */
 class ACCopy {
 	static active(node, param = {}){
 		return new Promise((resolve, reject) => {
-			let content = param.content;
-			let type = param.type || 'html';
-			if(!content){
+			if(!param.content){
 				throw "复制内容为空";
 			}
-			type === 'html' ? copyFormatted(content) : copy(content);
+			param.type === 'html' ? copyFormatted(param.content) : copy(param.content);
 			resolve();
 		});
 	}
--- dist/webcom.umd.js
+++ dist/webcom.umd.js
@@ -3866,16 +3866,16 @@
 	 * 参数：
 	 * *[data-copy-content]
 	 * *[data-copy-type] type 为 html 时表示复制内容为HTML
+	 * 使用举例：
+	 * <input type="button" value="复制链接” data-copy-content="http://abc.com" data-component="copy"/>
 	 */
 	class ACCopy {
 		static active(node, param = {}){
 			return new Promise((resolve, reject) => {
-				let content = param.content;
-				let type = param.type || 'html';
-				if(!content){
+				if(!param.content){
 					throw "复制内容为空";
 				}
-				type === 'html' ? copyFormatted(content) : copy(content);
+				param.type === 'html' ? copyFormatted(param.content) : copy(param.content);
 				resolve();
 			});
 		}
