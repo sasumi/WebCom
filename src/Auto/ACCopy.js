b345eecb873f680783307ccf02791ed60f83cd38
sasumi <cnsasumi@gmail.com>
2023-07-26 16:26:35 +0800

    upd

--- src/Auto/ACCopy.js
+++ src/Auto/ACCopy.js
@@ -5,16 +5,16 @@ import {copy, copyFormatted} from "../Widget/Copy.js";
  * 参数：
  * *[data-copy-content]
  * *[data-copy-type] type 为 html 时表示复制内容为HTML
+ * 使用举例：
+ * <input type="button" value="复制链接” data-copy-content="http://abc.com" data-component="copy"/>
  */
 export class ACCopy {
 	static active(node, param = {}){
 		return new Promise((resolve, reject) => {
-			let content = param.content;
-			let type = param.type || 'html';
-			if(!content){
+			if(!param.content){
 				throw "复制内容为空";
 			}
-			type === 'html' ? copyFormatted(content) : copy(content);
+			param.type === 'html' ? copyFormatted(param.content) : copy(param.content);
 			resolve();
 		});
 	}
