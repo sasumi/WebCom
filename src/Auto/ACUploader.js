c8d08dbaa29d04874a3854082cc9a23d3cb977b0
sasumi <cnsasumi@gmail.com>
2025-01-07 19:21:47 +0800

    upd

--- src/Auto/ACUploader.js
+++ src/Auto/ACUploader.js
@@ -4,10 +4,10 @@ import {objectKeyMapping} from "../Lang/Array.js";
 /**
  * 上传组件
  * 参数：
- * *[data-uploader-value]
- * *[data-uploader-name]
- * *[data-uploader-thumb]
- * *[data-uploader-uploadurl]
+ * *[data-uploader-value] 上传值
+ * *[data-uploader-name] 上传文件文件名
+ * *[data-uploader-thumb] 上传初始值缩略图地址
+ * *[data-uploader-uploadurl] 上传CGI
  * *[data-uploader-···] 更多参数请参考 Uploader.constructor()
  * 使用举例：
  * <input type="button" value="复制链接” data-copy-content="http://abc.com" data-component="copy"/>
